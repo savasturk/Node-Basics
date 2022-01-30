7036c477d634ba7f86847b07b011bd1af090abe8
savasturk <pinar.savasturk@ceng.deu.edu.tr>
2022-01-30 18:37:57 +0300

    started async

---
+++ async/hello-async.js
@@ -0,0 +1,23 @@
+/*setTimeout(function () {
+    console.log('1');
+}, 2000);
+
+setTimeout(function () {
+    console.log('3');
+}, 1000);
+
+console.log('2');*/
+
+console.log('Challenges: ');
+
+function printInTwoSeconds (message) {
+    setTimeout(function () {
+        console.log(message);
+    }, 2000);
+    // setTimeout 2000
+    //  console.to print message var
+}
+
+printInTwoSeconds('Hello async!');
+
+// API key : 7a75502995dd10a775158ab5f3acde1a
