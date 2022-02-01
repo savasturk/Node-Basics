d45f0b3f524d7f52ec13010b3476335a27bc72f3
savasturk <pinar.savasturk@ceng.deu.edu.tr>
2022-02-02 01:33:08 +0300

    beginner of api

---
+++ web-server/server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+var middleware = require('./middleware.js');
+
+//app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
+
+/*app.get('/', function (req, res) {
+    res.send('Hello Express!');
+});
+*/
+app.get('/about',middleware.requireAuthentication, function (req, res) {
+    res.send('About Us');
+});
+
+app.use(express.static(__dirname + '/public'));
+//console.log(__dirname);
+
+app.listen(PORT, function () {
+    console.log('Express server started on port ' + PORT + '!');
+});
